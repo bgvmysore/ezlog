1860d2717011af0f0510c6bba12e2c35f93dd1a4
Bhargav R <bgvmysore@gmail.com>
2023-06-25 19:33:45 +0530

    Macros were added to simplfy usage
    - added config macros
    - added LOG macros
    - Modifications to ezlog_Xprint() : added color_en parameter

--- ezlog.c
+++ ezlog.c
@@ -1,82 +1,115 @@
 #include "ezlog.h"
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#ifndef EZLOG_NO_COLORS
-    #define COLOR_TEXT_BEGIN(stream, x) (fprintf(stream, "\033[%dm", x))
-    #define COLOR_TEXT_END(stream)  (fprintf(stream, "\033[39m"))
-#else
-    #define COLOR_TEXT_BEGIN(stream, x)
-    #define COLOR_TEXT_END(stream)
-#endif
+#define COLOR_TEXT_BEGIN(stream, x) (fprintf(stream, "\033[%dm", x))
+#define COLOR_TEXT_END(stream)  (fprintf(stream, "\033[39m"))
+
+#define EZLOG_ERROR_PREFIX "[ERROR] "
+#define EZLOG_WARN_PREFIX  "[WARN] "
+#define EZLOG_INFO_PREFIX  "[INFO] "
+#define EZLOG_DEBUG_PREFIX "[DEBUG] "
 
 enum ezlog_term_colors {
-    EZLOG_RED    = 31,
-    EZLOG_GREEN  = 32,
-    EZLOG_YELLOW = 33,
-    EZLOG_BLUE   = 34,
-    EZLOG_DEFCOL = 39
+    EZLOG_COLOR_RED     = 31,
+    EZLOG_COLOR_GREEN   = 32,
+    EZLOG_COLOR_YELLOW  = 33,
+    EZLOG_COLOR_BLUE    = 34,
+    EZLOG_COLOR_DEFAULT = 39
 };
 
-int ezlog_eprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
+const enum ezlog_term_colors EZLOG_COLORS_TABLE[EZLOG_MAX_LOGLVL] = {
+    [EZLOG_ERROR] = EZLOG_COLOR_RED,
+    [EZLOG_WARNING] = EZLOG_COLOR_YELLOW,
+    [EZLOG_INFO] = EZLOG_COLOR_BLUE,
+    [EZLOG_DEBUG] = EZLOG_COLOR_GREEN
+};
 
+const char* EZLOG_PREFIX_TABLE[EZLOG_MAX_LOGLVL] = {
+    [EZLOG_ERROR] = EZLOG_ERROR_PREFIX,
+    [EZLOG_WARNING] = EZLOG_WARN_PREFIX,
+    [EZLOG_INFO] = EZLOG_INFO_PREFIX,
+    [EZLOG_DEBUG] = EZLOG_DEBUG_PREFIX
+};
+
+int ezlog_eprint(FILE* stream, bool color_en, const char* _fmt, ...) {
+    int result = 0;
+    const enum ezlog_levels lvl = EZLOG_ERROR;
     va_list args;
     va_start(args, _fmt);
 
-    COLOR_TEXT_BEGIN(stream, EZLOG_RED);
-    result += fprintf(stream, EZLOG_ERROR_PREFIX);
-    result += vfprintf(stream, _fmt, args);
-    COLOR_TEXT_END(stream);
+    if (color_en) {
+        COLOR_TEXT_BEGIN(stream, EZLOG_COLORS_TABLE[lvl]);
+    }
 
+    result += fprintf(stream, "%s", EZLOG_PREFIX_TABLE[lvl]);
+    result += vfprintf(stream, _fmt, args);
+    result += fprintf(stream, "\n");
+
+    if (color_en) { COLOR_TEXT_END(stream); }
+    va_end(args);
+
+    return result;
+
+}
+
+int ezlog_wprint(FILE* stream, bool color_en, const char* _fmt, ...) {
+    int result = 0;
+    const enum ezlog_levels lvl = EZLOG_WARNING;
+    va_list args;
+    va_start(args, _fmt);
+
+    if (color_en) {
+        COLOR_TEXT_BEGIN(stream, EZLOG_COLORS_TABLE[lvl]);
+    }
+
+    result += fprintf(stream, "%s", EZLOG_PREFIX_TABLE[lvl]);
+    result += vfprintf(stream, _fmt, args);
+    result += fprintf(stream, "\n");
+
+    if (color_en) { COLOR_TEXT_END(stream); }
     va_end(args);
 
     return result;
 }
 
-int ezlog_wprint(FILE* stream, const char* _fmt, ...) {
+int ezlog_iprint(FILE* stream, bool color_en, const char* _fmt, ...) {
     int result = 0;
-
+    const enum ezlog_levels lvl = EZLOG_INFO;
     va_list args;
     va_start(args, _fmt);
 
-    COLOR_TEXT_BEGIN(stream, EZLOG_YELLOW);
-    result += fprintf(stream, EZLOG_WARN_PREFIX);
-    result += vfprintf(stream, _fmt, args);
-    COLOR_TEXT_END(stream);
+    if (color_en) {
+        COLOR_TEXT_BEGIN(stream, EZLOG_COLORS_TABLE[lvl]);
+    }
 
+    result += fprintf(stream, "%s", EZLOG_PREFIX_TABLE[lvl]);
+    result += vfprintf(stream, _fmt, args);
+    result += fprintf(stream, "\n");
+
+    if (color_en) { COLOR_TEXT_END(stream); }
     va_end(args);
 
     return result;
 }
 
-int ezlog_iprint(FILE* stream, const char* _fmt, ...) {
+int ezlog_dprint(FILE* stream, bool color_en, const char* _fmt, ...) {
     int result = 0;
-
+    const enum ezlog_levels lvl = EZLOG_DEBUG;
     va_list args;
     va_start(args, _fmt);
 
-    COLOR_TEXT_BEGIN(stream, EZLOG_BLUE);
-    result += fprintf(stream, EZLOG_INFO_PREFIX);
+    if (color_en) {
+        COLOR_TEXT_BEGIN(stream, EZLOG_COLORS_TABLE[lvl]);
+    }
+
+    result += fprintf(stream, "%s", EZLOG_PREFIX_TABLE[lvl]);
     result += vfprintf(stream, _fmt, args);
-    COLOR_TEXT_END(stream);
+    result += fprintf(stream, "\n");
 
-    va_end(args);
-
-    return result;
-}
-
-int ezlog_dprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
-
-    va_list args;
-    va_start(args, _fmt);
-
-    COLOR_TEXT_BEGIN(stream, EZLOG_GREEN);
-    result += fprintf(stream, EZLOG_DEBUG_PREFIX);
-    result += vfprintf(stream, _fmt, args);
-    COLOR_TEXT_END(stream);
-
+    if (color_en) { COLOR_TEXT_END(stream); }
     va_end(args);
 
     return result;
--- ezlog.h
+++ ezlog.h
@@ -2,13 +2,22 @@
 #define _EZLOG_H_
 
 #include <stdio.h>
+#include <stdbool.h>
 
-#define EZLOG_VERSION "v1.0.0"
+#define EZLOG_VERSION "v1.1.0"
+#define EZLOG_MAX_LOGLVL (4U)
 
-#define EZLOG_ERROR_PREFIX "[ERROR] "
-#define EZLOG_WARN_PREFIX  "[WARN] "
-#define EZLOG_INFO_PREFIX  "[INFO] "
-#define EZLOG_DEBUG_PREFIX "[DEBUG] "
+#ifndef EZLOG_CONFIG_LOGSTREAM
+#define EZLOG_CONFIG_LOGSTREAM stdout
+#endif
+
+#ifndef EZLOG_CONFIG_COLOREN
+#define EZLOG_CONFIG_COLOREN (1)
+#endif
+
+#ifndef EZLOG_CONFIG_LOGLVL
+#define EZLOG_CONFIG_LOGLVL 3
+#endif
 
 enum ezlog_levels {
     EZLOG_ERROR   = 0,
@@ -17,12 +26,43 @@ enum ezlog_levels {
     EZLOG_DEBUG   = 3
 };
 
-int ezlog_eprint(FILE* stream, const char* _fmt, ...);
+#define EZLOGE(fmtstr, ...) ezlog_eprint(EZLOG_CONFIG_LOGSTREAM, EZLOG_CONFIG_COLOREN, fmtstr, ##__VA_ARGS__)
+#define EZLOGW(fmtstr, ...) ezlog_wprint(EZLOG_CONFIG_LOGSTREAM, EZLOG_CONFIG_COLOREN, fmtstr, ##__VA_ARGS__)
+#define EZLOGI(fmtstr, ...) ezlog_iprint(EZLOG_CONFIG_LOGSTREAM, EZLOG_CONFIG_COLOREN, fmtstr, ##__VA_ARGS__)
+#define EZLOGD(fmtstr, ...) ezlog_dprint(EZLOG_CONFIG_LOGSTREAM, EZLOG_CONFIG_COLOREN, fmtstr, ##__VA_ARGS__)
 
-int ezlog_wprint(FILE* stream, const char* _fmt, ...);
+#ifdef EZLOG_CONFIG_NOLOGS
+#undef EZLOGE
+#define EZLOGE(fmtstr, ...)
+#undef EZLOGW
+#define EZLOGW(fmtstr, ...)
+#undef EZLOGI
+#define EZLOGI(fmtstr, ...)
+#undef EZLOGD
+#define EZLOGD(fmtstr, ...)
+#endif
 
-int ezlog_iprint(FILE* stream, const char* _fmt, ...);
+#if EZLOG_CONFIG_LOGLVL <= 2
+#undef EZLOGD
+#define EZLOGD(fmtstr, ...)
+#endif
 
-int ezlog_dprint(FILE* stream, const char* _fmt, ...);
+#if EZLOG_CONFIG_LOGLVL <= 1
+#undef EZLOGI
+#define EZLOGI(fmtstr, ...)
+#endif
+
+#if EZLOG_CONFIG_LOGLVL <= 0
+#undef EZLOGW
+#define EZLOGW(fmtstr, ...)
+#endif
+
+int ezlog_eprint(FILE* stream, bool color_en, const char* _fmt, ...);
+
+int ezlog_wprint(FILE* stream, bool color_en, const char* _fmt, ...);
+
+int ezlog_iprint(FILE* stream, bool color_en, const char* _fmt, ...);
+
+int ezlog_dprint(FILE* stream, bool color_en, const char* _fmt, ...);
 
 #endif //_EZLOG_H_
