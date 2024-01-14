51b87516d9c03a210d800e87f28ba64099203c00
Bhargav R <bgvmysore@gmail.com>
2024-01-14 17:31:27 +0530

    ezlog v2: removed bloat and made it eazy
    Unnecessary code and complicated macros removed. The library
    source is much eazier to understand modify.
    
    Signed-off-by: Bhargav Raviprakash <bgvmysore@gmail.com>

--- ezlog.c
+++ ezlog.c
@@ -1,13 +1,13 @@
 #include "ezlog.h"
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 
 #define COLOR_TEXT_BEGIN(stream, x) (fprintf(stream, "\033[%dm", x))
-#define COLOR_TEXT_END(stream)  (fprintf(stream, "\033[39m"))
+#define COLOR_TEXT_END(stream)      (fprintf(stream, "\033[39m"))
 
 #define EZLOG_ERROR_PREFIX "[ERROR] "
 #define EZLOG_WARN_PREFIX  "[WARN] "
@@ -15,96 +15,48 @@
 #define EZLOG_DEBUG_PREFIX "[DEBUG] "
 
 enum ezlog_term_colors {
-    EZLOG_COLOR_RED     = 31,
-    EZLOG_COLOR_GREEN   = 32,
-    EZLOG_COLOR_YELLOW  = 33,
-    EZLOG_COLOR_BLUE    = 36,
-    EZLOG_COLOR_DEFAULT = 39
+	EZLOG_COLOR_RED     = 31,
+	EZLOG_COLOR_GREEN   = 32,
+	EZLOG_COLOR_YELLOW  = 33,
+	EZLOG_COLOR_BLUE    = 36,
+	EZLOG_COLOR_DEFAULT = 39,
 };
 
 const enum ezlog_term_colors EZLOG_COLORS_TABLE[EZLOG_MAX_LOGLVL] = {
-    [EZLOG_ERROR] = EZLOG_COLOR_RED,
-    [EZLOG_WARNING] = EZLOG_COLOR_YELLOW,
-    [EZLOG_INFO] = EZLOG_COLOR_BLUE,
-    [EZLOG_DEBUG] = EZLOG_COLOR_GREEN
+	[EZLOG_ERROR]   = EZLOG_COLOR_RED,
+	[EZLOG_WARNING] = EZLOG_COLOR_YELLOW,
+	[EZLOG_INFO]    = EZLOG_COLOR_BLUE,
+	[EZLOG_DEBUG]   = EZLOG_COLOR_GREEN,
 };
 
 const char* EZLOG_PREFIX_TABLE[EZLOG_MAX_LOGLVL] = {
-    [EZLOG_ERROR] = EZLOG_ERROR_PREFIX,
-    [EZLOG_WARNING] = EZLOG_WARN_PREFIX,
-    [EZLOG_INFO] = EZLOG_INFO_PREFIX,
-    [EZLOG_DEBUG] = EZLOG_DEBUG_PREFIX
+	[EZLOG_ERROR]   = EZLOG_ERROR_PREFIX,
+	[EZLOG_WARNING] = EZLOG_WARN_PREFIX,
+	[EZLOG_INFO]    = EZLOG_INFO_PREFIX,
+	[EZLOG_DEBUG]   = EZLOG_DEBUG_PREFIX,
 };
 
-static bool g_ezlog_time_en = false;
-static char g_time_buff[50] = {0};
+static FILE* g_ezlog_stream;
 static bool g_ezlog_color_en = true;
 
-#define EZLOG_FUNC_IMPL do { \
-    va_list args; \
-    va_start(args, _fmt); \
-    \
-    if (g_ezlog_color_en) { \
-      COLOR_TEXT_BEGIN(stream, EZLOG_COLORS_TABLE[lvl]); \
-    } \
-    \
-    if (g_ezlog_time_en) { \
-      ezlog_get_time_data(); \
-      result += fprintf(stream, "[%s] ", g_time_buff); \
-    } \
-    \
-    result += fprintf(stream, "%s", EZLOG_PREFIX_TABLE[lvl]); \
-    result += vfprintf(stream, _fmt, args); \
-    result += fprintf(stream, "\n"); \
-    \
-    if (g_ezlog_color_en) { COLOR_TEXT_END(stream); } \
-    va_end(args); \
-} while(0)
+int ezlog_print(enum ezlog_levels lvl, const char* _fmt, ...) {
+	int result     = 0;
+	g_ezlog_stream = stdout;
 
-static void ezlog_get_time_data() {
-    time_t current_time = time(NULL);
-    memset(g_time_buff, 0, sizeof(g_time_buff));
-    struct tm *local_time = localtime(&current_time);
-    strftime(g_time_buff, sizeof(g_time_buff), "%d-%m-%Y %H:%M:%S", local_time);
-}
+	va_list args;
+	va_start(args, _fmt);
 
-int ezlog_eprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
-    const enum ezlog_levels lvl = EZLOG_ERROR;
+	if (g_ezlog_color_en)
+		COLOR_TEXT_BEGIN(g_ezlog_stream, EZLOG_COLORS_TABLE[lvl]);
 
-    EZLOG_FUNC_IMPL;
+	result += fprintf(g_ezlog_stream, "%s", EZLOG_PREFIX_TABLE[lvl]);
+	result += vfprintf(g_ezlog_stream, _fmt, args);
+	result += fprintf(g_ezlog_stream, "\n");
 
-    return result;
-}
+	if (g_ezlog_color_en)
+		COLOR_TEXT_END(g_ezlog_stream);
 
-int ezlog_wprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
-    const enum ezlog_levels lvl = EZLOG_WARNING;
+	va_end(args);
 
-    EZLOG_FUNC_IMPL;
-
-    return result;
-}
-
-int ezlog_iprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
-    const enum ezlog_levels lvl = EZLOG_INFO;
-
-    EZLOG_FUNC_IMPL;
-
-    return result;
-}
-
-int ezlog_dprint(FILE* stream, const char* _fmt, ...) {
-    int result = 0;
-    const enum ezlog_levels lvl = EZLOG_DEBUG;
-
-    EZLOG_FUNC_IMPL;
-
-    return result;
-}
-
-void ezlog_init(bool time_en, bool color_en) {
-    g_ezlog_time_en = time_en;
-    g_ezlog_color_en = color_en;
+	return result;
 }
--- ezlog.h
+++ ezlog.h
@@ -1,70 +1,35 @@
 #ifndef _EZLOG_H_
 #define _EZLOG_H_
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define EZLOG_VERSION "v1.2.0"
-#define EZLOG_MAX_LOGLVL (4U)
-
-#ifndef EZLOG_CONFIG_COLOREN
-#define EZLOG_CONFIG_COLOREN (1U)
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-#ifndef EZLOG_CONFIG_LOGLVL
-#define EZLOG_CONFIG_LOGLVL (3U)
-#endif
-
-#ifndef EZLOG_TAG
-#define EZLOG_TAG ""
-#endif
+#define EZLOG_VERSION "v2.0.0"
 
 enum ezlog_levels {
-    EZLOG_ERROR   = 0,
-    EZLOG_WARNING = 1,
-    EZLOG_INFO    = 2,
-    EZLOG_DEBUG   = 3
+	EZLOG_ERROR = 0,
+	EZLOG_WARNING,
+	EZLOG_INFO,
+	EZLOG_DEBUG,
+	EZLOG_MAX_LOGLVL,
 };
 
-#define EZLOGE(fmtstr, ...) (void)ezlog_eprint(stdout, "%s: " fmtstr, (char*)EZLOG_TAG, ##__VA_ARGS__)
-#define EZLOGW(fmtstr, ...) (void)ezlog_wprint(stdout, "%s: " fmtstr, (char*)EZLOG_TAG, ##__VA_ARGS__)
-#define EZLOGI(fmtstr, ...) (void)ezlog_iprint(stdout, "%s: " fmtstr, (char*)EZLOG_TAG, ##__VA_ARGS__)
-#define EZLOGD(fmtstr, ...) (void)ezlog_dprint(stdout, "%s: " fmtstr, (char*)EZLOG_TAG, ##__VA_ARGS__)
+int ezlog_print(enum ezlog_levels loglvl, const char* _fmt, ...);
 
-#ifdef EZLOG_CONFIG_NOLOGS
-#undef EZLOGE
-#define EZLOGE(fmtstr, ...)
-#undef EZLOGW
-#define EZLOGW(fmtstr, ...)
-#undef EZLOGI
-#define EZLOGI(fmtstr, ...)
-#undef EZLOGD
-#define EZLOGD(fmtstr, ...)
+#define EZLOGE(fmtstr, ...) (void)ezlog_print(EZLOG_ERROR, fmtstr, ##__VA_ARGS__);
+
+#define EZLOGW(fmtstr, ...) (void)ezlog_print(EZLOG_WARNING, fmtstr, ##__VA_ARGS__);
+
+#define EZLOGI(fmtstr, ...) (void)ezlog_print(EZLOG_INFO, fmtstr, ##__VA_ARGS__);
+
+#define EZLOGD(fmtstr, ...) (void)ezlog_print(EZLOG_DEBUG, fmtstr, ##__VA_ARGS__);
+
+#ifdef __cplusplus
+}
 #endif
-
-#if EZLOG_CONFIG_LOGLVL <= 2
-#undef EZLOGD
-#define EZLOGD(fmtstr, ...)
-#endif
-
-#if EZLOG_CONFIG_LOGLVL <= 1
-#undef EZLOGI
-#define EZLOGI(fmtstr, ...)
-#endif
-
-#if EZLOG_CONFIG_LOGLVL <= 0
-#undef EZLOGW
-#define EZLOGW(fmtstr, ...)
-#endif
-
-void ezlog_init(bool time_en, bool color_en);
-
-int ezlog_eprint(FILE* stream, const char* _fmt, ...);
-
-int ezlog_wprint(FILE* stream, const char* _fmt, ...);
-
-int ezlog_iprint(FILE* stream, const char* _fmt, ...);
-
-int ezlog_dprint(FILE* stream, const char* _fmt, ...);
 
 #endif //_EZLOG_H_
